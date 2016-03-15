7bea15b43e7bba3459ee33a29c1a175d95ae177c
antochaveez <anto.chavez1993@gmail.com>
2016-03-14 23:40:47 -0300

    Creacion de abmlibros

---
+++ tableModale/LibroTableModel.java
@@ -0,0 +1,102 @@
+package tableModale;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.table.AbstractTableModel;
+
+import entidad.Editor;
+import entidad.Libro;
+
+public class LibroTableModel extends AbstractTableModel {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	List<Libro> libList = new ArrayList<>();
+	
+	
+	
+
+	@Override
+	public int getColumnCount() {
+		
+		return 6;
+	}
+
+	@Override
+	public int getRowCount() {
+		
+		return libList.size();
+	}
+	
+	public String getColumnName(int columnIndex) {
+
+		if (columnIndex == 0) {
+			return "Código";
+		}
+
+		if (columnIndex == 1) {
+			return "Descripción";
+		}
+
+		if (columnIndex == 2) {
+			return "Editorial";
+		}
+		if (columnIndex == 3) {
+			return "Autor";
+		}
+		if (columnIndex == 4) {
+			return "Isbn";
+		}
+		if (columnIndex == 5) {
+			return "Observación";
+		}
+
+		return "";
+	}
+
+	@Override
+	public Object getValueAt(int fila, int columna) {
+		Libro lib = libList.get(fila);
+			
+			if (columna == 0) {
+				return lib.getLibCodigo();
+			}
+	
+			if (columna == 1) {
+				return lib.getLibDescri().trim();
+	
+			}
+	
+			if (columna == 2) {
+				return lib.getEditor();
+	
+			}
+			if (columna == 3) {
+				return lib.getLibAutor();
+			}
+			if (columna == 4) {
+				return lib.getLibIsbn();
+			}
+			if (columna == 5) {
+				return lib.getLibObse().trim();
+			}
+			
+			
+			return lib;
+			
+		
+	}
+
+	public List<Libro> getLibList() {
+		return libList;
+	}
+
+	public void setLibList(List<Libro> libList) {
+		this.libList = libList;
+	}
+
+}
--- visual/AbmLibros.java
+++ visual/AbmLibros.java
@@ -5,12 +5,19 @@ import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.border.EmptyBorder;
-import java.awt.Rectangle;
+
+import tableModale.LibroTableModel;
+import javax.swing.JTable;
 
 public class AbmLibros extends JFrame {
 
 	private JPanel contentPane;
+	private LibroTableModel modelo = new LibroTableModel();
+	private int fila;
+	private JTable table;
+	
 
 	/**
 	 * Launch the application.
@@ -21,6 +28,7 @@ public class AbmLibros extends JFrame {
 				try {
 					AbmLibros frame = new AbmLibros();
 					frame.setVisible(true);
+					frame.setResizable(false);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -32,13 +40,21 @@ public class AbmLibros extends JFrame {
 	 * Create the frame.
 	 */
 	public AbmLibros() {
-		setBounds(new Rectangle(0, 0, 800, 600));
+		setTitle("Registrar Libros");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 900, 600);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(22, 70, 378, 359);
+		contentPane.add(scrollPane);
+		
+		table = new JTable();
+		scrollPane.setViewportView(table);
 	}
 
 }
