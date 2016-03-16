c94092fcac4f65786144fa1203d9cd650dc433eb
Mauricio <mauri.banger@gmail.com>
2016-03-16 11:36:52 -0300

    Creada la prueba para la configuracion

---
+++ test/PruebaConfiguracion.java
@@ -0,0 +1,64 @@
+package test;
+
+import java.util.List;
+
+import entidad.Config;
+import session.SessionConfiguracion;;
+
+public class PruebaConfiguracion {
+
+    public static void main(String[] args) throws Exception {
+        
+        //pruebaInsertar();
+        //pruebaModificar();
+        pruebaObtener();
+        pruebaCambiarConfiguracion();
+        pruebaObtener();
+        
+    }
+    
+    public static void pruebaInsertar() throws Exception {
+        Config config = new Config();
+        
+        config.setCfgDeumor(500);
+        config.setCfgDiamor(9);
+        config.setCfgDiapre(5);
+        config.setCfgOrga("UNE");
+        
+        
+        SessionConfiguracion.insertar(config);
+        
+    }
+    
+        
+    public static void pruebaModificar() throws Exception {
+        
+        Config config = new Config("UNE", 6000, 5, 9);
+         
+    
+        SessionConfiguracion.editar(config);
+    }
+    
+    public static void pruebaObtener() throws Exception {
+        
+        // Si existe la confifuracion
+        if (SessionConfiguracion.existeConfiguracion()){
+            Config config = SessionConfiguracion.obtenerConfiguracion();
+            System.out.println(config.getCfgOrga());
+        }else{
+            System.out.println("No existe configuracion");
+        }
+
+        
+    }
+    
+    public static void pruebaCambiarConfiguracion() throws Exception{
+        Config config = new Config();
+        config.setCfgOrga("Poli");
+        config.setCfgDeumor(5);
+        config.setCfgDiamor(5);
+        config.setCfgDiapre(8);
+        SessionConfiguracion.cambiarConfiguracion(config);
+    }
+
+}
